d8fecab32ad129cd5d7d662cf24904fa2635c099
Clément PREVOST <prevostclement@gmail.com>
2012-01-23 03:39:58 +0100

    capified

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :application, "pinkglasses.fr"
+set :repository,  "git://github.com/prevostc/pinkglasses.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :user, "pinkglasses"
+role :web, application                          # Your HTTP server, Apache/etc
+role :app, application                          # This may be the same as your `Web` server
+role :db,  application, :primary => true # This is where Rails migrations will run
+
+set :use_sudo, false
+set :deploy_to, "/var/www/#{application}"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
